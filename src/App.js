6cd2de0cf6be07e59c011be917eaa338659e73ae
adam-ubuntu <gy.adam90@gmail.com>
2020-10-20 10:58:28 -0700

    Fetch restaurant data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "./components/Table";
 import "./css/app.css";
 
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  function handleErrors(response) {
+    if (!response.ok) {
+      setError(true);
+      setLoading(false);
+      throw Error(response.statusText);
+    }
+  }
+
+  useEffect(() => {
+    async function getRestaurants() {
+      const url = "https://code-challenge.spectrumtoolbox.com/api/restaurants";
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Authorization: "Api-Key q3MNxtfep8Gt",
+          },
+        });
+        handleErrors(response);
+        try {
+          const data = await response.json();
+          setRestaurants(data);
+        } catch (error) {
+          console.error(error);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setError(true);
+        setLoading(false);
+      }
+    }
+
+    getRestaurants();
+  }, []);
+
   return (
     <div className="app">
       <header className="app-header">
